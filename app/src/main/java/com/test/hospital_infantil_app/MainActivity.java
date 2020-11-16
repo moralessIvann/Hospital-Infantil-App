c5607804f6ed603b4bb7db796e8251dc912fdf73
BryCabVi <36718676+BryCabVi@users.noreply.github.com>
2020-11-15 18:55:52 -0800

    implementacion1
    Implementación para visualizar archivos PDF

--- app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
+++ app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
@@ -3,6 +3,15 @@ package com.test.hospital_infantil_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PDFView pdfView = findViewById(R.id.pdfView);
+        pdfView.fitToWidth(0);
+        pdfView.fromAsset("ESV.pdf")
+                .pageFitPolicy(FitPolicy.BOTH)
+                .enableSwipe(true)
+                /*Set swipeH to false if vertical swipe is needed*/
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                /*.onPageChange((OnPageChangeListener) this)*/
+                .enableAnnotationRendering(true)
+                /*.onLoad((OnLoadCompleteListener) this)*/
+                /*Deja navegar por las paginas*/
+                .scrollHandle(new DefaultScrollHandle(this))
+                .enableAntialiasing(true)
+                .spacing(5)
+                .load();
     }
 }
