a5c37377556d65ea4b31dc9402414881bc20a565
Ivan <ivan.quevedo@uabc.edu.mx>
2020-11-23 17:25:38 -0800

    PDF en la aplicacion (sin botones)
    Ya pude abrir el pdf dentro de la aplicacion, siguietne fase seria agregar los botones por encima

--- app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
+++ app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
@@ -1,8 +1,9 @@
 package com.test.hospital_infantil_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("Buena alimentación para su bebé- version español.pdf")
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .load();
     }
 }
