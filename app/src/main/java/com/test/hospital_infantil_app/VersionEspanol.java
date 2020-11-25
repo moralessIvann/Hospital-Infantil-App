8dec50599902359f716aec3f3d15b0ba2b53126e
Ivan <ivan.quevedo@uabc.edu.mx>
2020-11-25 00:18:00 -0800

    Botones de Inicio
    Agrege Los botones para seleccionar entre espanol e ingles y los avances que llevaba bryan

--- app/src/main/java/com/test/hospital_infantil_app/VersionEspanol.java
+++ app/src/main/java/com/test/hospital_infantil_app/VersionEspanol.java
@@ -1,50 +1,35 @@
 package com.test.hospital_infantil_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
 
-public class MainActivity extends Activity {
+public class VersionEspanol extends Activity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.pdf_reader);
 
-    public void onSpanish(View view){
-        Intent intent = new Intent(this, VersionEspanol.class);
-        startActivity(intent);
-    }
 
-    public void onEnglish(View view){
-        Intent intent = new Intent(this, VersionIngles.class);
-        startActivity(intent);
-    }
-
-    /*
-    private void PDFReader(){
         PDFView pdfView = findViewById(R.id.pdfView);
         pdfView.fromAsset("Buena alimentación para su bebé- version español.pdf")
+                .pageFitPolicy(FitPolicy.BOTH)
                 .enableSwipe(true) // allows to block changing pages using swipe
                 .swipeHorizontal(true)
                 .enableDoubletap(true)
                 .defaultPage(0)
                 .enableAnnotationRendering(true) // render annotations (such as comments, colors or forms)
                 .password(null)
-                .scrollHandle(null)
+                .scrollHandle(new DefaultScrollHandle(this))
                 .enableAntialiasing(true) // improve rendering a little bit on low-res screens
                 // spacing between pages in dp. To define spacing color, set view background
-                .spacing(0)
+                .spacing(5)
                 .pageFitPolicy(FitPolicy.WIDTH)
                 .load();
-    }*/
+    }
 }
