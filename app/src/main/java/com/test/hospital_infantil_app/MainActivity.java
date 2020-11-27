8428473f0333d9be643f23d66b3497b1ed540d2d
BryCabVi <36718676+BryCabVi@users.noreply.github.com>
2020-11-27 03:20:19 -0800

    Interfaz
    -Cambio del manejo de la interfaz con modo oscuro/modo normal y modo vertical/horizontal.
    -Cambios estéticos.
    -Añadido botón para observar la información del hospital.

--- app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
+++ app/src/main/java/com/test/hospital_infantil_app/MainActivity.java
@@ -1,40 +1,31 @@
 package com.test.hospital_infantil_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 
-import com.github.barteksc.pdfviewer.PDFView;
-import com.github.barteksc.pdfviewer.listener.OnDrawListener;
-import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
-import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
-import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
-import com.github.barteksc.pdfviewer.util.FitPolicy;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PDFView pdfView = findViewById(R.id.pdfView);
-        pdfView.fitToWidth(0);
-        pdfView.fromAsset("ESV.pdf")
-                .pageFitPolicy(FitPolicy.BOTH)
-                .enableSwipe(true)
-                /*Set swipeH to false if vertical swipe is needed*/
-                .swipeHorizontal(true)
-                .enableDoubletap(true)
-                .defaultPage(0)
-                /*.onPageChange((OnPageChangeListener) this)*/
-                .enableAnnotationRendering(true)
-                /*.onLoad((OnLoadCompleteListener) this)*/
-                /*Deja navegar por las paginas*/
-                .scrollHandle(new DefaultScrollHandle(this))
-                .enableAntialiasing(true)
-                .spacing(5)
-                .load();
     }
+
+    public void onSpanish(View view){
+        Intent intent = new Intent(this, VersionEspanol.class);
+        startActivity(intent);
+    }
+
+    public void onEnglish(View view){
+        Intent intent = new Intent(this, VersionIngles.class);
+        startActivity(intent);
+    }
+
+    public void onInfo(View view) {
+        Intent intent = new Intent(this, Information.class);
+        startActivity(intent);
+    }
+
 }
